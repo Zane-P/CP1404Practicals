81ea652bf6e8781313c5b000d5d69580e8bb1a30
Zane <zanerpotter@gmail.com>
2020-03-15 21:40:32 +1000

    Add functions

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,14 +1,25 @@
-"""
-CP1404/CP5632 - Practical
-Fixed program to determine score status
-"""
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-elif score < 50:
-    print("Bad")
+
+def main():
+
+    def score_calculation(score):
+        result = ""
+        if score < 0 or score > 100:
+            result = "Invalid score"
+        elif score >= 90:
+            result = "Excellent"
+        elif score >= 50:
+            result = "Passable"
+        else:
+            result = "Bad"
+        return result
+
+    input_score = float(input("Enter score: "))
+    print(input_score, score_calculation(input_score))
+
+    random_score = float(random.randint(1, 100))
+    print(random_score, score_calculation(random_score))
+
+
+main()
