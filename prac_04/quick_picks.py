0d44e7b54e0b589e0a41ac6da0129212b0433d53
Zane <zanerpotter@gmail.com>
2020-03-22 22:21:28 +1000

    Initial commit

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,19 @@
+import random
+
+MIN_NUM = 1
+MAX_NUM = 45
+AMOUNT_OF_NUM = 6
+
+num_picks = int(input("How many quick picks? "))
+picks = []
+
+for i in range(num_picks):
+    picks.append([])
+    for n in range(AMOUNT_OF_NUM):
+        picks[i].append(random.randint(MIN_NUM, MAX_NUM))
+        while picks[i][n] in picks[i][:n]:
+            picks[i][n] = random.randint(MIN_NUM, MAX_NUM)
+    picks[i].sort()
+
+for i in range(num_picks):
+    print(picks[i])
